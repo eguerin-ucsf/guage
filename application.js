35d7f8df151f586a5713298f78d41dfe3784ed61
Jozef <jozefw@sbcglobal.net>
2016-05-16 16:51:31 -0700

    logic change

--- application.js
+++ application.js
@@ -1,13 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	setInterval(function(){
-		$("#needle").each(function(){
-			var a = Math.floor(Math.random() * 180 + 1);
-			$(this).css(
-				'transform', 'rotate(' + a + 'deg)');
-		});
+    setInterval(function() {
+        $("#needle").each(function() {
+            var a = Math.floor(Math.random() * 180 + 1);
+            if (a <= 60) {
+                $("#bkg").css('background-color', 'green');
+            }
+            else if(a >= 61 && a <= 120){
+            	$('#bkg').css('background-color','orange');
+
+            } else {$('#bkg').css('background-color','red')}
+
+
+            $(this).css(
+                'transform', 'rotate(' + a + 'deg)');
+        });
 
 
 
-	}, 1000);
+    }, 4000);
 });
